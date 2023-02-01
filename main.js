430010d80a86df11ebdc30584ee1bbf0ee77ae2f
Deepeshbabu <104300644+Deepeshbabu@users.noreply.github.com>
2023-02-01 20:26:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 video = "";
 input = "";
+stats = "";
+objects = [] ;
 
 function preload() 
 {
@@ -16,6 +18,30 @@ function setup()
 function draw() 
 {
     image(video,0,0,480,380) ;
+    if (stats != "") 
+    {
+        objectDetector.detect(video,gotResult);
+        for (let i = 0; i < objects.length; i++)
+        {
+                document.getElementById("status").innerHTML = "Status : Object Detected" ;
+
+                fill("FF0000") ;
+                percent = floor(objects[i].confidence * 100) ;
+                text(objects[i].label + " " + percent + "%",objects[i].x + 15, objects[i].y + 15);
+                noFill() ;
+                stroke("FF0000") ;
+                rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height) ;
+
+                if(object[i].label == input) 
+                {
+                    var synth = window.speechSynthesis ;
+                    speak_data = "Object Found" ;
+                    var utterThis = new SpeechSynthesisUtterance(speak_data) ;
+                    synth.speak(utterThis) ;
+                    document.getElementById("found").innerHTML = object[i].label + " Is Found" ;
+                }
+        }
+    }
 }
 
 function start() 
@@ -32,4 +58,17 @@ function modelLoaded()
     video.loop() ;
     video.speed(1);
     video.volume(0) ;
+}
+
+function gotResult(error, results) 
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        objects = results ;
+    }
 }
